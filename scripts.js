2fb12c1dbec89168b91a08232ee1c8864744bf44
Halim <46631802+HalimHamidov@users.noreply.github.com>
2021-03-03 13:02:46 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -9,14 +9,14 @@ const colors = {
 	ground: '#f4e7da',
 	rock: '#d5d5d4',
 	fairy: '#fceaff',
-/*	poison: '#98d7a5',
+	poison: '#98d7a5',
 	bug: '#f8d5a3',
 	dragon: '#97b3e6',
 	psychic: '#eaeda1',
 	flying: '#F5F5F5',
 	fighting: '#E6E0D4',
 	normal: '#F5F5F5'
-	*/
+	
 };
 const main_types = Object.keys(colors);
 
